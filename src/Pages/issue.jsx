19b5f2e9753226b4dc270b86b06f71f8a77d3bf1
Minxin-Shi <18222930184@163.COM>
2025-05-12 15:06:42 -0500

    trying to begin issure page

---
+++ src/Pages/issue.jsx
@@ -0,0 +1,9 @@
+<main className="flex">
+  <section className="w-2/3 p-6">
+    {/* Issues 区域（可静态） */}
+  </section>
+
+  <aside className="w-1/3 p-6 border-l">
+    <UpcomingMeetings />
+  </aside>
+</main>
