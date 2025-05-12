345ea7158ead0922dee8378f7ac2b58b4c5a57cb
zoryah-gray <zoryahgray2025@u.northwestern.edu>
2025-05-12 16:13:54 -0500

    added a basic profile page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import IssuePage from './Pages/issue';
+import ProfilePage from './Pages/profile';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<IssuePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
